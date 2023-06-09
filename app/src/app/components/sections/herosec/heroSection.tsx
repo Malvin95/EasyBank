4d118b87779cbb38060d926c7850c4edd01f40d6
Malvin Harding <Malvinharding@hotmail.co.uk>
2023-06-09 10:13:55 +0100

    started HeroSection work
    Started:
     - Working on the background section of the Hero Section.
     - Started working around the internal details of the Hero Section

--- app/src/app/components/sections/herosec/heroSection.tsx
+++ app/src/app/components/sections/herosec/heroSection.tsx
@@ -1,10 +1,25 @@
+import InviteButton from '../../inviteButton/inviteButton';
+import Image from 'next/image';
 import styles from './heroSection.module.css';
+import heroBackground from '../../../../../public/images/bg-intro-desktop.svg';
 
 export default function HeroSection() {
     return(
         <div className={styles.heroSectionContainer}>
-            <div></div>
-            <div></div>
+            {/* <Image className={styles.heroBackground} src={heroBackground} alt='heroBackground' /> */}
+            <div className={styles.leftHeroContainer}>
+                <div className={styles.lfhSubject}>
+                    <h1>Next generation digital banking</h1>
+                    <h5>
+                        Take your financial life online. Your Easybank account will be a one-stop-shop 
+                        for spending, saving, budgeting, investing, and much more.
+                    </h5>
+                    <div className={styles.btnContainer}>
+                        <InviteButton />
+                    </div>
+                </div>
+            </div>
+            <div className={styles.rightHeroContainer}></div>
         </div>
     )
 }
