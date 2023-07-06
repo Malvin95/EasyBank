377c924b139ad3cc94f28f8c61a074b9b541680d
Malvin Harding <Malvinharding@hotmail.co.uk>
2023-07-06 15:13:26 +0100

    'mostly' completed the hero section
    Work focused on:
    - Completed the design and set up of the Hero Section, may be subject to change in the future.
    
    - Minor Changes to the navbar and hero section background, to ensure that the design is consistent with what is in the UI

--- app/src/app/components/sections/herosec/heroSection.tsx
+++ app/src/app/components/sections/herosec/heroSection.tsx
@@ -1,25 +1,28 @@
 import InviteButton from '../../inviteButton/inviteButton';
 import Image from 'next/image';
 import styles from './heroSection.module.css';
-import heroBackground from '../../../../../public/images/bg-intro-desktop.svg';
+import imageMockup from '../../../../../public/images/image-mockups.png';
 
 export default function HeroSection() {
     return(
         <div className={styles.heroSectionContainer}>
-            {/* <Image className={styles.heroBackground} src={heroBackground} alt='heroBackground' /> */}
             <div className={styles.leftHeroContainer}>
                 <div className={styles.lfhSubject}>
-                    <h1>Next generation digital banking</h1>
-                    <h5>
-                        Take your financial life online. Your Easybank account will be a one-stop-shop 
-                        for spending, saving, budgeting, investing, and much more.
-                    </h5>
-                    <div className={styles.btnContainer}>
-                        <InviteButton />
+                    <div className={styles.lfhSubItem}>
+                        <p className={styles.heroSecHeader}>Next generation digital banking</p>
+                        <p className={styles.heroSecSubtitle}>
+                            Take your financial life online. Your Easybank account will be a one-stop-shop 
+                            for spending, saving, budgeting, investing, and much more.
+                        </p>
+                        <div className={styles.btnContainer}>
+                            <InviteButton />
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className={styles.rightHeroContainer}></div>
+            <div className={styles.rightHeroContainer}>
+                <Image className={styles.mockupOverlay} src={imageMockup} alt='imgMockups' />
+            </div>
         </div>
     )
 }
